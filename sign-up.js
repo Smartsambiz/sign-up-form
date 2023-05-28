732ec230a284e93899454a1cb251539e050dd01b
samuel <smartsam9981@gmail.com>
2023-05-28 19:31:55 +0100

    although i tried adding javascript to it so as to make the paswword section validated, it still didnt work, i am out of solutions, but i will be working on it as soon as i get hold of a solution

--- sign-up.js
+++ sign-up.js
@@ -1,15 +1,13 @@
-// Get the password and confirm password fields
 const passwordField = document.getElementById('password');
-const confirmPasswordField = document.getElementById('confirmPassword');
-
-// Function to validate password
-function validatePassword() {
+  const confirmPasswordField = document.getElementById('confirmPassword');
+  const errorText = document.getElementById('password-match-error');
+  
+  function validatePassword() {
     if (passwordField.value !== confirmPasswordField.value) {
-        confirmPasswordField.setCustomValidity("Passwords do not match");
-      } else {
-        confirmPasswordField.setCustomValidity("");
-      }
-}
-
-// Add event listener to the confirm password field
-confirmPasswordField.addEventListener('input', validatePassword);
+      errorText.style.display = 'block';
+    } else {
+      errorText.style.display = 'none';
+    }
+  }
+  
+  confirmPasswordField.addEventListener('input', validatePassword);
